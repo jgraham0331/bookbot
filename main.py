2ed64afe9109d311e1e1b5fefd6da0bfdc419329
jgraham0331 <jgraham0331@gmail.com>
2024-09-06 13:51:59 -0400

    added character count

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(num_words)
+
     letter_count = count_letters(text)
-    for k, v in letter_count.items():
-        print(k, ":", v)
+    letter_count.sort(reverse=True, key=sort_on)
+    
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document")
+    print("\n")
+
+    for i in letter_count:
+        print(f"The \'{i['character']}\' character was found {i['count']} times")
+
+    print("--- End report ---")
+
 
 def get_num_words(text):
     words = text.split()
@@ -19,11 +28,16 @@ def get_book_text(path):
 
 def count_letters(text):
     letters = sorted(" ".join(text.lower()).split())
+    letters = [i for i in letters if i.isalpha()]
     letter_count = {}
     for i in letters:
         if i in letter_count.keys():
             letter_count[i] += 1
         else:
             letter_count[i] =  1
-    return letter_count
+    return [{"character": k, "count": v} for k, v in letter_count.items()]
+
+def sort_on(dict):
+    return dict["count"]
+
 main()
